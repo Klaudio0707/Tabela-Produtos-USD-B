3ca802f57e277ed2aebe03ccff6811370f100a5e
*Bowie* <claudioroberto0707@outlook.com>
2025-02-04 10:20:42 -0300

    adicionando url do front no cors

--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ const productsRoutes = require('./routes/products');
 
 const app = express();
 const PORT = process.env.PORT;
-
+const FRONT_URL = process.env.FRONT_URL;
 
 // Middleware
 app.use(cors({
-    origin: "*", // Aceitando qualquer requisição
+    origin: FRONT_URL, // Aceitando qualquer requisição
     methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
     allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
   })
