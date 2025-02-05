c59f604a419c5ed1a0c6418d626cade1d25981ca
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-02-05 18:36:56 -0300

    removendo a url de desenvolvimento do cors

--- src/server.js
+++ src/server.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Importando as rotas de produtos
-const productsRoutes = require('./routes/products');
+const productsRoutes = require("./routes/products");
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONT_URL = process.env.FRONT_URL;
-const DESENVOLVIMENTO_URL = process.env.DESENVOLVIMENTO_URL;
 
 // Middleware
-app.use(cors({
-    origin: FRONT_URL || DESENVOLVIMENTO_URL, // Aceitando qualquer requisição
+app.use(
+  cors({
+    origin: FRONT_URL, // Aceitando Só o frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
     allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
   })
 );
 
-
 app.use(express.json());
 
 // Conectar ao MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('✅ Conectado ao MongoDB'))
-    .catch((error) => console.error('❌ Erro ao conectar ao MongoDB:', error));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Conectado ao MongoDB"))
+  .catch((error) => console.error("❌ Erro ao conectar ao MongoDB:", error));
 
 // Rotas de produtos
-app.use('/products', productsRoutes);
+app.use("/products", productsRoutes);
 
 // Middleware de erros globais
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Ocorreu um erro no servidor' });
+  console.error(err.stack);
+  res.status(500).json({ error: "Ocorreu um erro no servidor" });
 });
 
 // Iniciar o servidor
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-    console.log(`🌐 Frontend permitido: ${FRONT_URL}`);
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(`🌐 Frontend permitido: ${FRONT_URL}`);
 });
