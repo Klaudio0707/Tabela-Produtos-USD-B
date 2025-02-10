e030ddbdb6d1b8617cc55e17ee456bf747bfe110
*Bowie* <claudioroberto0707@outlook.com>
2025-02-10 16:50:20 -0300

    criação do authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,4 +2,24 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const user = require('../models/user');
 
-exports
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Senha incorreta' });
+
+        }
+        const token = jwt.sign({ id: user._id }, 'SeuSegredoJWT', { expiresIn: '2h' });
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({ message: 'Erro no servidor' });
+
+    }
+};
