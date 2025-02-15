8c01b6c7df6dc1133c84367e5c5e34082cf1a06b
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-02-14 22:31:24 -0300

    atualizando o padrão do mongoDB

--- src/database/database.js
+++ src/database/database.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose"); // importando a biblioteca do mongoose
 const connectDB = async () => { // função assíncrona
   try { //tenta
     await mongoose.connect(process.env.MONGO_URI, { // conecta ao banco 
-      useNewUrlParser: true, //aceita as novas funções
-      useUnifiedTopology: true, // 
+  
     });
     console.log("Conectado ao MongoDB");
   } catch (error) { // caso dê errado, mostra o erro
